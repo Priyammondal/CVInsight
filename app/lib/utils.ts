fc8fb5afe74e76351d286530a8b9e91010bd5e14
Priyammondal <priyammondal096@gmail.com>
2026-01-15 18:29:57 +0530

    push

---
+++ app/lib/utils.ts
@@ -0,0 +1,9 @@
+export function formatSize(bytes: number): string {
+    if (bytes === 0) return "0 bytes";
+    const k = 1014;
+    const size: string[] = ['Bytes', 'KB', 'MB', 'GB', 'TB'];
+    const i: number = Math.floor(Math.log(bytes) / Math.log(k));
+    return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + " " + size[i];
+}
+
+export const generateUUID = () => crypto.randomUUID();
---
+++ app/routes/resume.tsx
@@ -0,0 +1,41 @@
+import React, { useEffect } from 'react'
+import { Link, useParams } from 'react-router'
+
+export const meta = () => ([
+    { title: "CVInsight | Review" },
+    { name: "descriptioin", content: "Detailed overview of your resume" }
+])
+
+const resume = () => {
+    const { id } = useParams();
+
+    useEffect(() => {
+        const loadResume = async () => {
+            
+        }
+    }, [id])
+
+    return (
+        <main className='!pt-0'>
+            <nav className="resume-nav">
+                <Link to="/" className='back-button'>
+                    <img src="/icons/back.svg" alt="logo" className='w-2.5 h-2.5' />
+                    <span className='text-gray-800 text-sm font-semibold'>Back to Homepage</span>
+                </Link>
+            </nav>
+            <div className="flex flex-row w-full max-lg:flex-col-reverse">
+                <section className="feedback-section">
+                    {
+                        imageUrl && resumeUrl && (
+                            <div className='animate-in fade-in duration-1000 gradient-border max-sm:m-0 h-[90%] max-wxl:h-fit w-fit'>
+
+                            </div>
+                        )
+                    }
+                </section>
+            </div>
+        </main>
+    )
+}
+
+export default resume
